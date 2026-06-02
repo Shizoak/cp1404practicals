7f4c1d61cb2b0a27c2377a86f57f1b6cae503216
Takuma <takuma.matsunami@my.jcu.edu.au>
2026-06-03 01:34:55 +1000

    Add more_scores.py into prac_02

---
+++ prac_02/more_scores.py
@@ -0,0 +1,31 @@
+"""
+Practice Work, More Scores
+"""
+from random import randint
+
+
+def main():
+    number_of_score = int(input("Number of Scores: "))
+    results = open("results.txt", "w")
+    for i in range(number_of_score):
+        random_score = get_random_score()
+        random_grade = determine_grade(random_score)
+        print(f"{random_score} is {random_grade}", file=results)
+    results.close()
+
+
+def determine_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def get_random_score():
+    return randint(0, 100)
+
+main()
